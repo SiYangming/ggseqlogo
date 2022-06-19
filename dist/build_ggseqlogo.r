e91aee539360bc2d8023d46fa7746e6ce4fffe4b
SiYangming <yangming-6863@qq.com>
2022-06-19 14:29:01 +0800

    Update build_ggseqlogo.r
    Fixed the following message caused by ggplot2 updates.
    Warning message:
    `guides(<scale> = FALSE)` is deprecated. Please use `guides(<scale> = "none")` instead.

--- dist/build_ggseqlogo.r
+++ dist/build_ggseqlogo.r
@@ -2,7 +2,8 @@ setwd('~/Development/ggseqlogo/')
 require(devtools)
 remove.packages('ggseqlogo')
 R_PATH = '/usr/local/bin/R'
-targz = 'ggseqlogo_0.1.tar.gz'
+#targz = 'ggseqlogo_0.1.tar.gz'
+targz = 'ggseqlogo_0.1.1.tar.gz'
 system(sprintf('rm -rf dist/%s', targz))
 
 # Regenerate Rwd files
